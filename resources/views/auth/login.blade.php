572dd81e63aea624ea53b62f86c64690c2f668d3
Pablo Perotti <pabloperotti@Air-de-Pablo.fibertel.com.ar>
2023-06-22 23:37:56 -0300

    mods@

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,6 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.auth-master')
 
-<head>
-  <title>login</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('assets/login-estilos.css')}}">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&display=swap" rel="stylesheet">
-</head>
-
+@section('content')
 <body>
     <main>
         <div class="background">
@@ -24,7 +9,7 @@
                 <p>No tenes cuenta? <a href="./register">Registrarse</a></p>
             </div>
             <div class="box">
-                <form class="form" method="POST" action="/login">
+                <form class="form" method="POST" action="./login">
                   @csrf
                     <input type="text" name="username" class="usuario" placeholder="Usuario" required>
                     <input type="password" name="password" class="password" placeholder="Contraseña" required>
@@ -33,15 +18,6 @@
             </div>
         </div>
     </main>
+    @endsection
 
-  <!-- Bootstrap JavaScript Libraries -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
-</body>
-
-</html>
+  
--- resources/views/layouts/auth-master.blade.php
+++ resources/views/layouts/auth-master.blade.php
@@ -5,21 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
+    <link rel="stylesheet" href="{{asset('assets/login-estilos.css')}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&display=swap" rel="stylesheet">
     <title></title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="{{ url('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    
     
 </head>
 <body>
-  @include('layout.partials.navbar')
-<main class="container">
+
+  <main class="container">
   @yield('content')
 </main>
 
-</body>
-    
-
-    
+<script src="{{ url('assets/js/bootstrap.bundle.min.js')}}"></script>
+</body>    
 </html>
