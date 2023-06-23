cc2b6e52e86a46a58ced2a579cef9dd2c172ac26
Pablo Perotti <pabloperotti@Air-de-Pablo.fibertel.com.ar>
2023-06-22 23:07:50 -0300

    modificaciones

--- resources/views/layouts/auth-master.blade.php
+++ resources/views/layouts/auth-master.blade.php
@@ -6,40 +6,20 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.87.0">
-    <title>Signin Template · Bootstrap v5.1</title>
+    <title></title>
 
     <!-- Bootstrap core CSS -->
-    <link href="{!! url('assets/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
-    <link href="{!! url('assets/css/signin.css') !!}" rel="stylesheet">
+    <link href="{{ url('assets/css/bootstrap.min.css') }}" rel="stylesheet">
     
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
 </head>
-<body class="text-center">
-    
-    <main class="form-signin">
-
-        @yield('content')
-        
-    </main>
-    
+<body>
+  @include('layout.partials.navbar')
+<main class="container">
+  @yield('content')
+</main>
 
 </body>
+    
+
+    
 </html>
