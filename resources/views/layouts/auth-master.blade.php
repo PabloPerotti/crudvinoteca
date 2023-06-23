4acdc1dc01675a511d40c6a09532aa314dc44f1a
Pablo Perotti <pabloperotti@Air-de-Pablo.fibertel.com.ar>
2023-06-22 23:11:37 -0300

    mods

--- resources/views/layouts/auth-master.blade.php
+++ resources/views/layouts/auth-master.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.87.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
     <title></title>
 
     <!-- Bootstrap core CSS -->
